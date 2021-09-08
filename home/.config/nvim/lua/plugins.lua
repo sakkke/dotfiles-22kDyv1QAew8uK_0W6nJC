a0e4f45108b505bdbe584810fa6ec2d03f673b90
sakkke <w32w64@gmail.com>
2021-09-08 13:48:08 +0900

    :sparkles: add augroup

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -6,6 +6,13 @@ end
 
 vim.cmd [[packadd packer.nvim]]
 
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]]
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use {
