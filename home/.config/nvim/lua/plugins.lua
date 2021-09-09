4b9b3a4f94bc05131de4302d9f1aa267396f0026
sakkke <w32w64@gmail.com>
2021-09-09 16:55:30 +0900

    :package: add gitsigns.nvim

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -81,6 +81,15 @@ return require('packer').startup(function()
     requires = {'kyazdani42/nvim-web-devicons', opt = true},
   }
   use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end,
+    requires = {
+      'nvim-lua/plenary.nvim',
+    },
+  }
+  use {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
       vim.cmd [[
