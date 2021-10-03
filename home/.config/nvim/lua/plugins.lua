74c0bc32b62d660143c9f8b45e38509a06d99fbd
sakkke <w32w64@gmail.com>
2021-10-03 11:04:33 +0900

    :sparkles: update `g:previm_custom_css_paths`

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -532,6 +532,7 @@ return require('packer').startup({function()
     config = function()
       -- ref: https://milligram.io/#getting-started
       vim.g.previm_custom_css_paths = {
+        'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/stackoverflow-light.min.css',
         'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
         'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css',
         'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css',
