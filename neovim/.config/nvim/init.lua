84319a42eafd0ad0ce8ebe36887e89544992e2f0
sakkke <w32w64@gmail.com>
2022-03-27 20:12:54 +0900

    add `packer.nvim`

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,2 +1,4 @@
+require 'plugins'
 vim.o.number = true
 vim.o.relativenumber = true
+vim.cmd[[colorscheme catppuccin]]
---
+++ neovim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,48 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+  use({
+    "catppuccin/nvim",
+    as = "catppuccin"
+  })
+  use {
+    'feline-nvim/feline.nvim',
+    config = function()
+      require('feline').setup {
+        components = require 'catppuccin.core.integrations.feline'
+      }
+    end,
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+          require('gitsigns').setup()
+        end
+      },
+    },
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
