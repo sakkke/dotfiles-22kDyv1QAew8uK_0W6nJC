2506a2aa60bc4597a9bd3dfa35abfdff51eeff29
sakkke <w32w64@gmail.com>
2022-03-28 13:41:08 +0900

    set `list` to `true`

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,6 @@
 require 'plugins'
+vim.o.list = true
+vim.o.listchars = 'eol:↵,extends:»,nbsp:*,precedes:«,space:·,tab:»-,trail:-'
 vim.o.number = true
 vim.o.relativenumber = true
 vim.cmd[[silent! colorscheme catppuccin]]
