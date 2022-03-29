bb912300ea47c635e1a0f83e7adc3286a401b2a6
sakkke <w32w64@gmail.com>
2022-03-29 10:44:05 +0900

    add keymap `jj`

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,4 +3,6 @@ vim.o.list = true
 vim.o.listchars = 'eol:↵,extends:»,nbsp:*,precedes:«,space:·,tab:»-,trail:-'
 vim.o.number = true
 vim.o.relativenumber = true
+vim.api.nvim_set_keymap('c', 'jj', '<C-c>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true })
 vim.cmd[[silent! colorscheme catppuccin]]
