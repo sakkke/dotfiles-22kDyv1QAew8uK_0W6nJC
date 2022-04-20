15a9efa61bea8f3d574a33b8c4cc67150dc9ac40
sakkke <w32w64@gmail.com>
2022-04-21 06:24:09 +0900

    change keymaps

--- neovim.archlinux/.config/nvim/init.lua
+++ neovim.archlinux/.config/nvim/init.lua
@@ -3,8 +3,8 @@ vim.o.list = true
 vim.o.listchars = 'eol:↵,extends:»,nbsp:*,precedes:«,space:·,tab:»-,trail:-'
 vim.o.number = true
 vim.o.relativenumber = true
-vim.api.nvim_set_keymap('v', '<Leader>vB', [[c<C-r>=system('base64 -d', @")<CR><Esc>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<Leader>vb', [[c<C-r>=system('base64 -w0', @")<CR><Esc>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>B', [[c<C-r>=system('base64 -d', @")<CR><Esc>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>b', [[c<C-r>=system('base64 -w0', @")<CR><Esc>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('c', 'jj', '<C-c>', { noremap = true })
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true })
 vim.cmd[[silent! colorscheme catppuccin]]
