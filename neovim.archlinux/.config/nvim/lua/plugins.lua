630333392e1f456ae38706fdc1c493b7ea6b96d9
sakkke <w32w64@gmail.com>
2022-05-17 22:18:11 +0900

    update the configuration of `Autosave.nvim`

--- neovim.archlinux/.config/nvim/lua/plugins.lua
+++ neovim.archlinux/.config/nvim/lua/plugins.lua
@@ -21,7 +21,30 @@ return require('packer').startup({function(use)
   -- My plugins here
   -- use 'foo1/bar1.nvim'
   -- use 'foo2/bar2.nvim'
-  use "Pocco81/AutoSave.nvim"
+  use({
+    "Pocco81/AutoSave.nvim",
+    config = function()
+      local autosave = require("autosave")
+
+      autosave.setup(
+        {
+          enabled = true,
+          execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+          events = {"InsertLeave", "TextChanged"},
+          conditions = {
+            exists = true,
+            filename_is_not = {},
+            filetype_is_not = {},
+            modifiable = true
+          },
+          write_all_buffers = false,
+          on_off_commands = true,
+          clean_command_line_interval = 0,
+          debounce_delay = 135
+        }
+      )
+    end,
+  })
   use({
     "sakkke/catppuccin-nvim", -- Forked from "catppuccin/nvim"
     as = "catppuccin",
